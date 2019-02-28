bb6e7167926293ead74581d9cf35476505890a3c
Florian Hermann <florian.hermann5@etu.univ-lorraine.fr>
2019-02-28 17:25:24 +0100

    Update RouterForComplexRoutes.php
    Fix missing constant call

--- src/Middleware/RouterForComplexRoutes.php
+++ src/Middleware/RouterForComplexRoutes.php
@@ -47,7 +47,7 @@ class RouterForComplexRoutes implements MiddlewareInterface
             throw new \LogicException(sprintf('Invalid route config for route "%s". "%s" expected.', $parameters['_route'], self::CONTROLLER_METHOD_SEPARATOR));
         }
         
-        list($class, $method) = explode('::', $parameters['_controller'], 2);
+        list($class, $method) = explode(self::CONTROLLER_METHOD_SEPARATOR, $parameters['_controller'], 2);
         if (!$this->container->has($class)) {
             throw new \LogicException(sprintf('Invalid route config for route "%s". Controller "%s" not found.', $parameters['_route'], $class));
         }
