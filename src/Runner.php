be490b4d1119ef084c9040b69531db8a9644468b
Florian <florian.hermann94@gmail.com>
2019-04-13 10:49:27 +0200

    Runner middlewares dependencies as a variadic function

--- src/Runner.php
+++ src/Runner.php
@@ -21,7 +21,7 @@ class Runner implements RequestHandlerInterface
     /** @var MiddlewareInterface[] */
     private $queue;
 
-    public function __construct(iterable $queue)
+    public function __construct(MiddlewareInterface ...$queue)
     {
         $this->queue = $queue;
     }
